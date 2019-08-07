c45e87abcb70e4078107c01dc06e29a079368775
Gurlabe <Gurlabe72@gmail.com>
2019-08-07 13:49:40 -0700

    first commit, created a beginning form component.

---
+++ src/form/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <form>
+                <h1>Our form example</h1>
+                <div>
+                    <textarea />
+                </div>
+                <div>
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+        );
+    }
+}
+
+export default App;
